4eecea8ca50006b5be4a5efed46d042dbd72c87b
Ruslan Osipov <corrinevn0s@gmail.com>
2025-09-22 02:00:29 +0300

    Добавьте файлы проекта.

---
+++ tgBotReceptLapshi/Models/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace tgBotReceptLapshi.Models
+{
+    public record Dish
+    {
+        public Guid ID { get; } = Guid.NewGuid();
+        public string Name { get; init; }
+        public string Look {  get; init; }
+        public string Recipe { get; init; }
+    }
+}
